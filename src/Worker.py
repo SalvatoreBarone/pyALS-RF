a5ce80eb306b63d1d659091296b076454d6b3085
Salvatore <salvator.barone@gmail.com>
2021-12-06 21:03:20 +0100

    feature: default values for configuration parameters

--- src/Worker.py
+++ src/Worker.py
@@ -17,7 +17,8 @@ Street, Fifth Floor, Boston, MA 02110-1301, USA.
 import os, argparse, configparser
 from pyosys import libyosys as ys
 from .Configs import *
-from .Optimizer import *
+from .OneStepOptimizer import *
+from .SecondStepOptimizer import *
 
 
 class Worker:
@@ -47,26 +48,33 @@ class Worker:
       self.__classifier.dump()
       exit()
 
-    self.__classifier.generate_implementations(self.__output_dir)
+    self.__classifier.generate_hdl_exact_implementations(self.__output_dir)
     if (self.__ax_conf.technique == AxConfig.Technique.PS):
-      optimizer = Optimizer(self.__ax_conf.technique, self.__classifier, self.__test_dataset, self.__final_opt_conf)
+      optimizer = OneStepOptimizer(self.__ax_conf.technique, self.__classifier, self.__test_dataset, self.__final_opt_conf)
       optimizer.optimize()
       optimizer.plot_pareto(self.__output_dir + self.__pareto_view)
       optimizer.get_report(self.__output_dir + self.__report_file)
-      self.__classifier.generate_ps_ax_implementations(self.__output_dir, optimizer.get_individuals())
+      self.__classifier.generate_hdl_ps_ax_implementations(self.__output_dir, optimizer.get_individuals())
     else:
       if self.__ax_conf.strategy == AxConfig.Strategy.ONE_STEP:
-        optimizer = Optimizer(self.__ax_conf.technique, self.__classifier, self.__test_dataset, self.__final_opt_conf)
+        optimizer = OneStepOptimizer(self.__ax_conf.technique, self.__classifier, self.__test_dataset, self.__final_opt_conf)
         optimizer.optimize()
         optimizer.plot_pareto(self.__output_dir + self.__pareto_view)
         optimizer.get_report(self.__output_dir + self.__report_file)
         if (self.__ax_conf.technique == AxConfig.Technique.ALS):
-          self.__classifier.generate_asl_ax_implementations(self.__output_dir, optimizer.get_individuals())
+          self.__classifier.generate_hdl_onestep_asl_ax_implementations(self.__output_dir, optimizer.get_individuals())
         elif(self.__ax_conf.technique == AxConfig.Technique.FULL):
-          self.__classifier.generate_full_ax_implementations(self.__output_dir, optimizer.get_individuals())
+          self.__classifier.generate_hdl_onestep_full_ax_implementations(self.__output_dir, optimizer.get_individuals())
       else:
-        pass
-
+        self.__classifier.generate_first_step_ax_assertions(self.__test_dataset, self.__output_dir, self.__als_opt_conf)
+        optimizer = SecondStepOptimizer(self.__ax_conf.technique, self.__classifier, self.__test_dataset, self.__final_opt_conf)
+        optimizer.optimize()
+        optimizer.plot_pareto(self.__output_dir + self.__pareto_view)
+        optimizer.get_report(self.__output_dir + self.__report_file)
+        if (self.__ax_conf.technique == AxConfig.Technique.ALS):
+          self.__classifier.generate_hdl_twostep_asl_ax_implementations(self.__output_dir, optimizer.get_individuals())
+        elif (self.__ax_conf.technique == AxConfig.Technique.FULL):
+          self.__classifier.generate_hdl_twostep_full_ax_implementations(self.__output_dir, optimizer.get_individuals())
     print("All done! Take a look at the ", self.__output_dir, " directory.")
 
   def __cli_parser(self):
@@ -83,52 +91,34 @@ class Worker:
     self.__pmml_file = args.pmml
     self.__test_dataset = args.dataset
     self.__output_dir = args.output
-    print("Dump:               ", self.__print_tree)
-    print("Config file         ", self.__config_file)
-    print("PMML file:          ", self.__pmml_file)
-    print("Test dataset:       ", self.__test_dataset)
-    print("Outoput directory:  ", self.__output_dir)
 
   def __config_parser(self):
     config = configparser.ConfigParser()
     config.read(self.__config_file)
-    self.__ax_conf = AxConfig(config["approximation"]["technique"], config["approximation"]["strategy"])
-    self.__als_conf = ALSConfig(config["als"]["luttech"], config["als"]["catalog"], config["als"]["timeout"])
-    self.__final_opt_conf = NSGAConfig(int(config["final-optimization"]["population_size"]),
-                                       int(config["final-optimization"]["iterations"]),
-                                       float(config["final-optimization"]["crossover_probability"]),
-                                       float(config["final-optimization"]["crossover_eta"]),
-                                       float(config["final-optimization"]["mutation_probability"]),
-                                       float(config["final-optimization"]["mutation_eta"]),
-                                       float(config["final-optimization"]["max_accuracy_loss"]))
+    self.__ax_conf = AxConfig(
+      config["approximation"]["technique"],
+      config["approximation"]["strategy"])
 
-    self.__als_opt_conf = NSGAConfig(int(config["als-optimization"]["population_size"]),
-                                     int(config["als-optimization"]["iterations"]),
-                                     float(config["als-optimization"]["crossover_probability"]),
-                                     float(config["als-optimization"]["crossover_eta"]),
-                                     float(config["als-optimization"]["mutation_probability"]),
-                                     float(config["als-optimization"]["mutation_eta"]),
-                                     float(config["als-optimization"]["max_error_frequency"]))
+    self.__als_conf = ALSConfig(
+      config["als"]["luttech"] if "luttech" in config["als"] else "6",
+      config["als"]["catalog"] if "catalog" in config["als"] else "lut_catalog.db",
+      config["als"]["timeout"] if "timeout" in config["als"] else 60000,)
 
-    print("Technique:          ", self.__ax_conf.technique)
-    print("Strategy:           ", self.__ax_conf.strategy)
+    self.__final_opt_conf = NSGAConfig(
+      int(config["final-optimization"]["population_size"]) if "population_size" in config["final-optimization"] else 0,
+      int(config["final-optimization"]["iterations"]) if "iterations" in config["final-optimization"] else 123,
+      float(config["final-optimization"]["crossover_probability"]) if "crossover_probability" in config["final-optimization"] else 0.9,
+      float(config["final-optimization"]["crossover_eta"]) if "crossover_eta" in config["final-optimization"] else 1,
+      float(config["final-optimization"]["mutation_probability"]) if "mutation_probability" in config["final-optimization"] else 0,
+      float(config["final-optimization"]["mutation_eta"]) if "mutation_eta" in config["final-optimization"] else 20,
+      float(config["final-optimization"]["max_accuracy_loss"]) if "max_accuracy_loss" in config["final-optimization"] else 5)
 
-    print("ALS LUTs:           ", self.__als_conf.luttech)
-    print("ALS catalog:        ", self.__als_conf.catalog)
-    print("ALS timeout:        ", self.__als_conf.timeout)
+    self.__als_opt_conf = NSGAConfig(
+      int(config["als-optimization"]["population_size"]) if "population_size" in config["als-optimization"] else 0,
+      int(config["als-optimization"]["iterations"]) if "iterations" in config["als-optimization"] else 123,
+      float(config["als-optimization"]["crossover_probability"]) if "crossover_probability" in config["als-optimization"] else 0.9,
+      float(config["als-optimization"]["crossover_eta"]) if "crossover_eta" in config["als-optimization"] else 1,
+      float(config["als-optimization"]["mutation_probability"]) if "mutation_probability" in config["als-optimization"] else 0,
+      float(config["als-optimization"]["mutation_eta"]) if "mutation_eta" in config["als-optimization"] else 20,
+      float(config["als-optimization"]["max_error_frequency"]) if "max_error_frequency" in config["als-optimization"] else 10)
 
-    print("NSGA-II pop.size:   ", self.__final_opt_conf.pop_size)
-    print("NSGA-II iterations: ", self.__final_opt_conf.iterations)
-    print("NSGA-II Pcross:     ", self.__final_opt_conf.cross_p)
-    print("NSGA-II Ncross:     ", self.__final_opt_conf.cross_eta)
-    print("NSGA-II Pmut:       ", self.__final_opt_conf.mut_p)
-    print("NSGA-II Nmut:       ", self.__final_opt_conf.mut_eta)
-    print("NSGA-II max. error: ", self.__final_opt_conf.max_error)
-
-    print("NSGA-II pop.size:   ", self.__als_opt_conf.pop_size)
-    print("NSGA-II iterations: ", self.__als_opt_conf.iterations)
-    print("NSGA-II Pcross:     ", self.__als_opt_conf.cross_p)
-    print("NSGA-II Ncross:     ", self.__als_opt_conf.cross_eta)
-    print("NSGA-II Pmut:       ", self.__als_opt_conf.mut_p)
-    print("NSGA-II Nmut:       ", self.__als_opt_conf.mut_eta)
-    print("NSGA-II max. error: ", self.__als_opt_conf.max_error)
