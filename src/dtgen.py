bb66d7b4350bbe4ce160fad1254cbda6fbc38db6
Salvatore Barone <salvatore.barone@unina.it>
2023-07-20 18:58:21 +0200

    script

--- src/dtgen.py
+++ src/dtgen.py
@@ -55,29 +55,13 @@ def get_labels(outcomes, config):
 def get_sets(dataset_file, config, fraction):
     if config.outcome_col is None:
         config.outcome_col = -1
-    n_classes = len(config.classes_name)
     attributes, outcomes = read_dataset_from_csv(dataset_file, config.separator, config.skip_header, config.outcome_col)
     labels = np.array(get_labels(outcomes, config))
     labels.reshape((attributes.shape[0],)) 
-    # outcomes_one_hot = np.array(to_one_hot(outcomes, config.classes_name))
-    # outcomes_one_hot.reshape((attributes.shape[0], n_classes))
-    # outcomes = np.array(outcomes)
-    # outcomes.reshape((attributes.shape[0],))
     learning_set_size = int(np.ceil(attributes.shape[0] * fraction))
     learning_vectors_indexes = random.sample(range(int(attributes.shape[0])), learning_set_size)
     test_vectors_indexes = [x for x in range(attributes.shape[0]) if x not in learning_vectors_indexes]
-    learning_attributes = attributes[learning_vectors_indexes, :]
-    test_attributes = attributes[test_vectors_indexes, :]
-    if isinstance(config.classes_name, dict):
-        learning_labels = [ config.classes_name[o] for o in labels[learning_vectors_indexes] ]
-        test_labels = [ config.classes_name[o] for o in labels[test_vectors_indexes] ]
-    elif isinstance(config.classes_name, (list, tuple)):
-        # learning_labels = [ config.classes_name[int(o)] for o in outcomes[learning_vectors_indexes] ]
-        # test_labels = [ config.classes_name[int(o)] for o in outcomes[test_vectors_indexes] ]
-        learning_labels = labels[learning_vectors_indexes]
-        test_labels = labels[test_vectors_indexes]
-    #test_labels_one_hot = outcomes_one_hot[test_vectors_indexes, :]
-    return learning_attributes, learning_labels, test_attributes, test_labels
+    return attributes[learning_vectors_indexes, :], labels[learning_vectors_indexes], attributes[test_vectors_indexes, :], labels[test_vectors_indexes]
 
 def save_test_dataset_to_csv(filename, attributes_name, test_attributes, test_labels):
     original_stdout = sys.stdout
@@ -100,75 +84,29 @@ def graphviz_export(model, attributes_name, classes_name, outputdir):
         graph.render(directory = f"{outputdir}/export", filename = 'tree.gv')
     
 def dtgen(clf, dataset, configfile, outputdir, fraction, depth, predictors, criterion, min_sample_split, min_samples_leaf, max_features, max_leaf_nodes, min_impurity_decrease, ccp_alpha, disable_bootstrap):
-
     config = DtGenConfigParser(configfile)
-
     learning_attributes, learning_labels, test_attributes, test_labels = get_sets(dataset, config, fraction)
-
     if clf == "dt":
-        model = tree.DecisionTreeClassifier(
-                max_depth = depth,
-                criterion = criterion,
-                min_samples_split = min_sample_split,
-                min_samples_leaf = min_samples_leaf,
-                max_features = max_features,
-                max_leaf_nodes = max_leaf_nodes,
-                min_impurity_decrease = min_impurity_decrease,
-                ccp_alpha = ccp_alpha).fit(learning_attributes, learning_labels)
+        model = tree.DecisionTreeClassifier(max_depth = depth, criterion = criterion, min_samples_split = min_sample_split, min_samples_leaf = min_samples_leaf, max_features = max_features, max_leaf_nodes = max_leaf_nodes, min_impurity_decrease = min_impurity_decrease, ccp_alpha = ccp_alpha).fit(learning_attributes, learning_labels)
     elif clf == "rf":
-        model = ensemble.RandomForestClassifier(
-                n_estimators = predictors,
-                max_depth = depth,
-                criterion = criterion,
-                min_samples_split = min_sample_split,
-                min_samples_leaf = min_samples_leaf,
-                max_features = max_features,
-                max_leaf_nodes = max_leaf_nodes,
-                min_impurity_decrease = min_impurity_decrease,
-                ccp_alpha = ccp_alpha,
-                bootstrap = not disable_bootstrap,
-                n_jobs = -1,
-                verbose = 1).fit(list(learning_attributes), list(learning_labels))
+        model = ensemble.RandomForestClassifier(n_estimators = predictors, max_depth = depth, criterion = criterion, min_samples_split = min_sample_split, min_samples_leaf = min_samples_leaf, max_features = max_features, max_leaf_nodes = max_leaf_nodes, min_impurity_decrease = min_impurity_decrease, ccp_alpha = ccp_alpha, bootstrap = not disable_bootstrap, n_jobs = -1, verbose = 1).fit(list(learning_attributes), list(learning_labels))
     elif clf == "wc":
-        st = tree.DecisionTreeClassifier(
-                max_depth = depth,
-                criterion = criterion,
-                min_samples_split = min_sample_split,
-                min_samples_leaf = min_samples_leaf,
-                max_features = max_features,
-                max_leaf_nodes = max_leaf_nodes,
-                min_impurity_decrease = min_impurity_decrease,
-                ccp_alpha = ccp_alpha).fit(learning_attributes, learning_labels)
-        model = ensemble.RandomForestClassifier(
-                n_estimators = predictors,
-                max_depth = depth,
-                criterion = criterion,
-                min_samples_split = min_sample_split,
-                min_samples_leaf = min_samples_leaf,
-                max_features = max_features,
-                max_leaf_nodes = max_leaf_nodes,
-                min_impurity_decrease = min_impurity_decrease,
-                ccp_alpha = ccp_alpha,
-                bootstrap = not disable_bootstrap,
-                n_jobs = -1,
-                verbose = 1).fit(list(learning_attributes), list(learning_labels))
+        st = tree.DecisionTreeClassifier( max_depth = depth, criterion = criterion, min_samples_split = min_sample_split, min_samples_leaf = min_samples_leaf, max_features = max_features, max_leaf_nodes = max_leaf_nodes, min_impurity_decrease = min_impurity_decrease, ccp_alpha = ccp_alpha).fit(learning_attributes, learning_labels)
+        model = ensemble.RandomForestClassifier( n_estimators = predictors, max_depth = depth, criterion = criterion, min_samples_split = min_sample_split, min_samples_leaf = min_samples_leaf, max_features = max_features, max_leaf_nodes = max_leaf_nodes, min_impurity_decrease = min_impurity_decrease, ccp_alpha = ccp_alpha, bootstrap = not disable_bootstrap, n_jobs = -1, verbose = 1).fit(list(learning_attributes), list(learning_labels))
         for i in range(len(model.estimators_)):
             model.estimators_[i] = st
-        
     print(f"Classification accuracy: {sum(pred == ans for pred, ans in zip(model.predict(test_attributes), test_labels)) / len(test_labels)}")
     mkpath(outputdir)
-    
     pmml_file = f"{outputdir}/{clf}_{predictors}.pmml" if predictors > 1 else f"{outputdir}/{clf}.pmml"
     print(f"Exporting PMML model to {pmml_file} ...")
     pipe = pipeline.Pipeline([('clf', model)])
     skl_to_pmml(pipeline = pipe, col_names = config.attributes_name, pmml_f_name = pmml_file )
     # pipe = sklearn2pmml.PMMLPipeline([("classifier", model)])
     # sklearn2pmml.sklearn2pmml(pipe, pmml_file, with_repr = True)
-    
     test_dataset_csv = f"{outputdir}/test_dataset_4_pyALS-rf.csv"
     print(f"Exporting the test dataset to {test_dataset_csv} ...")
     save_test_dataset_to_csv(test_dataset_csv, config.attributes_name, test_attributes, test_labels)
-    print(f"Exporting graphviz draws of leaned trees to {outputdir}/export ...")
-    graphviz_export(model, config.attributes_name, list(config.classes_name.values()) if isinstance(config.classes_name, dict) else config.classes_name, outputdir)
+    # print(f"Exporting graphviz draws of leaned trees to {outputdir}/export ...")
+    # graphviz_export(model, config.attributes_name, list(config.classes_name.values()) if isinstance(config.classes_name, dict) else config.classes_name, outputdir)
     print("Done!")
     
