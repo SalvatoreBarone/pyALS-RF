dcb24623cd8b5872ba1db76b54e4e36431fd8490
AntonioEmmanuele <antony.35.ae@gmail.com>
2023-12-13 10:07:14 +0100

    feat: adds correlation comp

--- src/Flows/lcor_flow.py
+++ src/Flows/lcor_flow.py
@@ -49,42 +49,80 @@ from ..Model.Classifier import Classifier
 #                     for db, out in zip(dbs_b, not_nodes_b):
 #                         print(db["box"].feature_name, out, db["box"].threshold)
 #       exit()
-                
+
+# Compute the number of samples in each leaf
+# The func returns a data structure indexed by [leaf_class,tree,leaf].
+# Each element [i,j,z] contains (test_input,value) that falls into the leaf z.
 def samples_per_leaves(classifier : Classifier):
     samples_per_leaves_dict = { c : {t.name : {  l["sop"] : [] for l in t.leaves if l["class"] == c } for t in classifier.trees } for c in classifier.model_classes}
     for x, y in  tqdm(zip(classifier.x_test, classifier.y_test), total=len(classifier.x_test), desc="Computing dataset partitions", bar_format="{desc:30} {percentage:3.0f}% |{bar:40}{r_bar}{bar:-10b}", leave=False):
         for tree in classifier.trees:
-            boxes_output = tree.get_boxes_output(x)
+            boxes_output = tree.get_boxes_output(x) # Get decision boxes
             for class_name, assertions in tree.class_assertions.items():
                 for leaf in assertions:
-                    if eval(leaf, boxes_output):
+                    if eval(leaf, boxes_output): # if the Assertion is true then save (x,y) in the DS
                         samples_per_leaves_dict[class_name][tree.name][leaf].append((x, y))
     return samples_per_leaves_dict
-
+# Compute the correlation between each couple of leaves.
+# returns a structure indexed by [(tree_A,leaf_A),(tree_B,leaf_B)] such that:
+# DS[(tree_A,leaf_A),(tree_B,leaf_B)] =  correlation(leaf_a,leaf_b)
 def compute_leaves_correlation(samples_per_leaves_dict):
+    corr_per_leaf = {}
     for cls, cls_data in samples_per_leaves_dict.items():
-        for comb in combinations(cls_data.keys(), 2):
+        for comb in combinations(cls_data.keys(), 2): # for each couple of tree.
             tree_A = comb[0]
             tree_B = comb[1]
-            
-            N11, N00, N10, N01, N1X, NX1, N0X, NX0 = 0, 0, 0, 0, 0, 0, 0, 0
             for leaves in samples_per_leaves_dict[cls][tree_A], samples_per_leaves_dict[cls][tree_B]:
+                N11, N00, N1X, NX1, N0X, NX0 = 0, 0, 0, 0, 0, 0, 0, 0
+                Q_stat = 0
                 leaf_A = leaves[0]
                 leaf_B = leaves[1]
+                #The set here is the set of (x,y) present in at least leaf_A or leaf_B without repetition.
                 for xy in set(samples_per_leaves_dict[cls][tree_A][leaf_A] + samples_per_leaves_dict[cls][tree_B][leaf_B]):
                     y = xy[1]
                     if xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
-                        # Case 1:
+                        # Case 1: x,y is in both leaves and y == leaf_label i.e. prediction is correct 
                         N11 += 1
                     elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
-                        # Case 2:
+                        # Case 2: x,y is in both leaves and y != leaf_label i.e. prection is incorrect.
                         N00 += 1
-                        
-                    # elif 
-                        
-                        
+                    elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
+                        # Case 3: x,y is only in leaf_A and y == leaf_label.
+                        N1X += 1
+                    elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
+                        # Case 4: x,y is only in leaf_A and y != leaf_label.
+                        N0X += 1
+                    elif xy not in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy  in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
+                        # Case 5: x,y is only in leaf_B and y == leaf_label.
+                        NX1 += 1
+                    elif xy not in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
+                        # Case 6: x,y is only in leaf_B and y != leaf_label.
+                        NX0 += 1
+                n_conc = N11*N00
+                n_disc = N1X*NX1*N0X*NX0
+                Q_stat = (n_conc - n_disc) / (n_conc + n_disc)
+                corr_per_leaf[(tree_A,leaf_A)] = {(tree_B,leaf_B) : Q_stat}
+    return corr_per_leaf
                         
             
+# Algorithm(classifier, sample_per_leaf,corr_per_leaf,T',max_loss):
+# 
+# score_per_leaf = compute_score(corr_per_leaf, classifier)
+# base_accuracy = ComputeAccuracy(classifier,T')
+# actual_accuracy = base_accuracy 
+# scores_idx = 0
+# while base_accuracy - actual_accuracy > max_loss and scores_idx < len(score_per_leaf) :
+#   best_score = score_per_leaf[0]
+#   helper_classifier = prune(best_score, classifier)
+#   actual_score = ComputeAccuracy(classifier,T')
+#   if base_accuracy - actual_accuracy >= max_loss:
+#       classifier = helper_classifier
+#       Update_Correlation(corr_per_leaf)
+#       Update_Scores(score_per_leaf)
+#       scores_idx = 0 
+#   else 
+#       scores_idx ++
+# Problemi: indice scores_idx -> aggiornamento delle correlazioni mi porta a dover ricalcolare gli score, indice va resettato?
 
 def leaves_correlation_flow(ctx, output):
     logger = logging.getLogger("pyALS-RF")
