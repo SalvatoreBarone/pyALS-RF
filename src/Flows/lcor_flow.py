052d0cd395fced2e236580484be585c132346fe3
Antonio <antony.35.ae@gmail.com>
2023-12-18 15:36:53 +0100

    feat: adds score

--- src/Flows/lcor_flow.py
+++ src/Flows/lcor_flow.py
@@ -56,19 +56,58 @@ from ..Model.Classifier import Classifier
 def samples_per_leaves(classifier : Classifier):
     # samples_per_leaves_dict = { c : {t.name : {  l["sop"] : [] for l in t.leaves if l["class"] == c } for t in classifier.trees } for c in classifier.model_classes}
     samples_per_leaves_dict = { c : {t.name : {  l["sop"] : set() for l in t.leaves if l["class"] == c } for t in classifier.trees } for c in classifier.model_classes} 
-    number_of_leaves = 0
+    #total_set = set()
+    #s = 0
+    #classified = [False for i in range(0,len(classifier.x_test))]
     for x, y in  tqdm(zip(classifier.x_test, classifier.y_test), total=len(classifier.x_test), desc="Computing dataset partitions", bar_format="{desc:30} {percentage:3.0f}% |{bar:40}{r_bar}{bar:-10b}", leave=False):
         for tree in classifier.trees:
             boxes_output = tree.get_boxes_output(x) # Get decision boxes
             for class_name, assertions in tree.class_assertions.items():
                 for leaf in assertions:
-                    number_of_leaves += 1
                     if eval(leaf, boxes_output): # if the Assertion is true then save (x,y) in the DS
                         # print(f'Leaf name {leaf}')
                         # print(f'Sample, class {(tuple(frozenset(x)),y[0])}')
                         # samples_per_leaves_dict[class_name][tree.name][leaf].add((x, y))
+                        #classified[s] = True
                         samples_per_leaves_dict[class_name][tree.name][leaf].add((tuple(frozenset(x)),y[0]))
-    print(f'N.ro leaves : {number_of_leaves}')
+                        #total_set.add(tuple(frozenset(x)))
+        #s += 1
+    # # To delete...
+    # number_of_leaves = 0
+    # for tree in classifier.trees:
+    #     boxes_output = tree.get_boxes_output(x) # Get decision boxes
+    #     for class_name, assertions in tree.class_assertions.items():
+    #         for leaf in assertions:
+    #             number_of_leaves += 1
+    # print(f'N.ro leaves : {number_of_leaves} total number of classified samples {len(total_set)}')
+    # print(f'number of samples {len(classifier.x_test)}') 
+    # print(f'Counted samples {s}')
+    # counted_classified = 0
+    # for c in classified:
+    #     if c == True :
+    #         counted_classified += 1
+    # print(f'Counted classified {counted_classified}')
+    # reps = 0
+    # for i in range(0,len(classifier.x_test)):
+    #     for j in range(0,len(classifier.x_test)):
+    #         if np.array_equal(classifier.x_test[i],classifier.x_test[j]) and i != j:
+    #             reps += 1
+    # print(f' Repetitions {reps} Total: {reps+len(total_set)}')
+    
+    # counter = 0
+    # counter_wrong = 0
+    # for cls, cls_data in samples_per_leaves_dict.items():
+    #     for tree,tree_data in cls_data.items():
+    #         for leaf,leaf_data in tree_data.items():
+    #             for sample in leaf_data:
+    #                 # print(sample)
+    #                 # print(sample[1])
+    #                 if sample[1] != int(cls):
+    #                     counter_wrong += 1
+    #                     print(f'Leaf {cls} Correct {sample[1]}')
+    #                     print(f' Type CLS : {type(cls)} Type sample : {type(sample[1])}')
+    #                 counter += 1
+    # print(f'Total {counter} Wrong {counter_wrong}')
     return samples_per_leaves_dict
 
 # Compute the correlation between each couple of leaves.
@@ -76,109 +115,105 @@ def samples_per_leaves(classifier : Classifier):
 # DS[(tree_A,leaf_A),(tree_B,leaf_B)] =  correlation(leaf_a,leaf_b)
 def compute_leaves_correlation(samples_per_leaves_dict):
     corr_per_leaf = {}
-    conc = 0
-    disc = 0
-    for cls, cls_data in samples_per_leaves_dict.items():
-        for tree,tree_data in cls_data.items():
-            for leaf,leaf_data in tree_data.items():
-                print(leaf_data)
-                # if len(leaf_data) == 0:
-                #     disc += 1
-                # else :
-                #     conc += 1
-
+    corr_per_leaf_cleaned = {}
     for cls, cls_data in samples_per_leaves_dict.items(): # for each class
         for comb in combinations(cls_data.keys(), 2): # for each couple of tree.
             tree_A = comb[0] # take the pair of tree 
             tree_B = comb[1]
-            #for leaves in samples_per_leaves_dict[cls][tree_A], samples_per_leaves_dict[cls][tree_B]:
-            # counter = 0 
             for leaves in product(list(samples_per_leaves_dict[cls][tree_A].keys()),list(samples_per_leaves_dict[cls][tree_B].keys())):
-                N11, N00, N1X, NX1, N0X, NX0 = 0, 0, 0, 0, 0, 0
+                #N11, N00, N1X, NX1, N0X, NX0 = 0, 0, 0, 0, 0, 0
+                conc, disc = 0, 0
                 Q_stat = 0
-                #print(leaves)
                 leaf_A = leaves[0]
                 leaf_B = leaves[1]
 
-                # print(f'Leaf A: {leaf_A}')
-                # print(f'Leaf B: {leaf_B}')
-                # if leaf_A in samples_per_leaves_dict[cls][tree_A].keys():
-                #     print("OOOOK ")
-                # else:
-                #     print(" FUCK ")
-                # if leaf_B in samples_per_leaves_dict[cls][tree_A].keys():
-                #     print("OOOOK ")
-                # else:
-                #     print(" FUCK ")
-                # print(f'Samples in leaf A: {samples_per_leaves_dict[cls][tree_A][leaf_A]}')
-                # print(f'Samples in leaf B: {samples_per_leaves_dict[cls][tree_B][leaf_B]}')
-                # if(len(samples_per_leaves_dict[cls][tree_A][leaf_A]) > 0 and len(samples_per_leaves_dict[cls][tree_B][leaf_B]) > 0 ):
-                #     conc += 1
-                # elif  :
-                #     disc += 1
-                
-                #exit()
-                
-                # print(f'{type(samples_per_leaves_dict[cls][tree_A][leaf_A])}')
-                # if(len(samples_per_leaves_dict[cls][tree_A][leaf_A]) > 0 and len(samples_per_leaves_dict[cls][tree_B][leaf_B]) > 0 ) :
-                #     print(type(samples_per_leaves_dict[cls][tree_A][leaf_A][0])) 
-                #     print(type(samples_per_leaves_dict[cls][tree_A][leaf_A][0][0])) 
-                #     print(samples_per_leaves_dict[cls][tree_A][leaf_A][0][0]) 
-                #     print(type(samples_per_leaves_dict[cls][tree_A][leaf_A][0][1]))
-                #     print(samples_per_leaves_dict[cls][tree_A][leaf_A][0][1]) 
+                # if len(samples_per_leaves_dict[cls][tree_A][leaf_A]) > 0 and len(samples_per_leaves_dict[cls][tree_B][leaf_B]) > 0 :
+                #     # The set here is the set of (x,y) present in at least leaf_A or leaf_B without repetition.
+                #     for xy in samples_per_leaves_dict[cls][tree_A][leaf_A] | samples_per_leaves_dict[cls][tree_B][leaf_B]:   
+                #         y = xy[1]
+                #         if xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
+                #             # Case 1: x,y is in both leaves and y == leaf_label i.e. prediction is correct 
+                #             N11 += 1
+                #         elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
+                #             # Case 2: x,y is in both leaves and y != leaf_label i.e. prection is incorrect.
+                #             N00 += 1
+                #         elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
+                #             # Case 3: x,y is only in leaf_A and y == leaf_label.
+                #             N1X += 1
+                #         elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
+                #             # Case 4: x,y is only in leaf_A and y != leaf_label.
+                #             N0X += 1
+                #         elif xy not in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy  in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
+                #             # Case 5: x,y is only in leaf_B and y == leaf_label.
+                #             NX1 += 1
+                #         elif xy not in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
+                #             # Case 6: x,y is only in leaf_B and y != leaf_label.
+                #             NX0 += 1
+                #     if N11 > 0 or N1X > 0 or NX1 >0 :
+                #         print("************************")
+                #         print(f'N11: {N11} N00: {N00}')
+                #         print(f'N0X: {N0X} NX0: {NX0}')
+                #         print(f'N1X: {N1X} NX1: {NX1}')
+                #         print("************************")
 
-                #     print(type(samples_per_leaves_dict[cls][tree_B][leaf_B][0])) 
-                #     print(type(samples_per_leaves_dict[cls][tree_B][leaf_B][0][0])) 
-                #     print(samples_per_leaves_dict[cls][tree_B][leaf_B][0][0]) 
-                #     print(type(samples_per_leaves_dict[cls][tree_B][leaf_B][0][1]))
-                #     print(samples_per_leaves_dict[cls][tree_B][leaf_B][0][1]) 
+                if len(samples_per_leaves_dict[cls][tree_A][leaf_A]) > 0 and len(samples_per_leaves_dict[cls][tree_B][leaf_B]) > 0 :
+                    # The set here is the set of (x,y) present in at least leaf_A or leaf_B without repetition.
+                    for xy in samples_per_leaves_dict[cls][tree_A][leaf_A] | samples_per_leaves_dict[cls][tree_B][leaf_B]:   
+                        y = xy[1]
+                        if xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B]:
+                            # Case 1: x,y is in both leaves 
+                            conc += 1
+                        elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B]:
+                            #  Case 2: x,y is in leafA but not in leafB
+                            disc += 1
+                        elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B]:
+                            #  Case 3: x,y is in leafB but not in leafA
+                            disc += 1   
+                    # print(f'TreeA {tree_A} leafA {leaf_A} treeB {tree_B} leafB {leaf_B}')
+                    # print(f'Conc {conc} disc {disc}') 
+                    Q_stat = (conc  -  disc) / (conc + disc)
+                    # print(f'Qstat {Q_stat}')
+                    if (tree_A,leaf_A) not in corr_per_leaf:
+                        corr_per_leaf[(tree_A,leaf_A)] = {(tree_B,leaf_B) : Q_stat}
+                    else:
+                        corr_per_leaf[(tree_A,leaf_A)].update({(tree_B,leaf_B) : Q_stat})
+                    # Compute the dual in order to remove the leaf with a greater number of literals.
+                    if (tree_B,leaf_B) not in corr_per_leaf:
+                        corr_per_leaf[(tree_B,leaf_B)] = {(tree_A,leaf_A) : Q_stat}
+                    else:
+                        corr_per_leaf[(tree_B,leaf_B)].update({(tree_A,leaf_A) : Q_stat})
+                    if Q_stat > 0:
+                        if (tree_A,leaf_A) not in corr_per_leaf_cleaned:
+                            corr_per_leaf_cleaned[(tree_A,leaf_A)] = {(tree_B,leaf_B) : Q_stat}
+                        else:
+                            corr_per_leaf_cleaned[(tree_A,leaf_A)].update({(tree_B,leaf_B) : Q_stat})
+                        # Compute the dual in order to remove the leaf with a greater number of literals.
+                        if (tree_B,leaf_B) not in corr_per_leaf_cleaned:
+                            corr_per_leaf_cleaned[(tree_B,leaf_B)] = {(tree_A,leaf_A) : Q_stat}
+                        else:
+                            corr_per_leaf_cleaned[(tree_B,leaf_B)].update({(tree_A,leaf_A) : Q_stat})
+        
+    #print(f'conc {conc} disc {disc} total number of samples {len(samples_set)}')
+    return corr_per_leaf,corr_per_leaf_cleaned
+                    
+# Compute the score for each leaf multiplying the sum(Q_stat(A,B))* Num literals.
+def compute_leaves_score(corr_per_leaf):
+    scores = {}
+    for leafA in corr_per_leaf:
+        scores[leafA] = 0
+    for leafA,correlated in corr_per_leaf.items():
+        num_and = 0
+        for leaves,Q_val in correlated.items():
+            scores[leafA] += Q_val
+        splitted = leafA[1].split()
+        for and_w in splitted:
+            if and_w == "and":
+                num_and += 1
+        #print(f'Name {leafA[1]} num {num_and}')
+        scores[leafA] = scores[leafA] * num_and
+    # multiply by the number of literals.
+    return scores
 
-                #     counter += 1 
-                #     if counter == 10:
-                #         exit()
-                
-                # for xy in samples_per_leaves_dict[cls][tree_A][leaf_A] :
-                #     is_present = False
-                #     # check if the class is on the other vector
-                #     for xy1 in sample   
-
-                      
-                # The set here is the set of (x,y) present in at least leaf_A or leaf_B without repetition.
-                #for xy in samples_per_leaves_dict[cls][tree_A][leaf_A] + samples_per_leaves_dict[cls][tree_B][leaf_B]:   
-                # for xy in np.union1d(samples_per_leaves_dict[cls][tree_A][leaf_A], samples_per_leaves_dict[cls][tree_B][leaf_B]):
-                #    y = xy[1]
-                #     if xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
-                #         # Case 1: x,y is in both leaves and y == leaf_label i.e. prediction is correct 
-                #         N11 += 1
-                #     elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
-                #         # Case 2: x,y is in both leaves and y != leaf_label i.e. prection is incorrect.
-                #         N00 += 1
-                #     elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
-                #         # Case 3: x,y is only in leaf_A and y == leaf_label.
-                #         N1X += 1
-                #     elif xy in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy not in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
-                #         # Case 4: x,y is only in leaf_A and y != leaf_label.
-                #         N0X += 1
-                #     elif xy not in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy  in samples_per_leaves_dict[cls][tree_B][leaf_B] and y == cls:
-                #         # Case 5: x,y is only in leaf_B and y == leaf_label.
-                #         NX1 += 1
-                #     elif xy not in samples_per_leaves_dict[cls][tree_A][leaf_A] and xy in samples_per_leaves_dict[cls][tree_B][leaf_B] and y != cls:
-                #         # Case 6: x,y is only in leaf_B and y != leaf_label.
-                #         NX0 += 1
-                # n_conc = N11*N00
-                # n_disc = N1X*NX1*N0X*NX0
-                # #if len(samples_per_leaves_dict[cls][tree_A][leaf_A]) == 0 and len(samples_per_leaves_dict[cls][tree_B][leaf_B]) == 0 :
-                # if n_conc == 0 or n_disc == 0:
-                #    Q_stat = 0
-                # else :
-                #     Q_stat = (n_conc - n_disc) / (n_conc + n_disc)
-                # #print(Q_stat)
-                #exit()
-                # corr_per_leaf[(tree_A,leaf_A)] = {(tree_B,leaf_B) : Q_stat}
-    print(f'conc {conc} disc {disc}')
-    return corr_per_leaf
-                        
-            
 # Algorithm(classifier, sample_per_leaf,corr_per_leaf,T',max_loss):
 # 
 # score_per_leaf = compute_score(corr_per_leaf, classifier)
@@ -214,7 +249,17 @@ def leaves_correlation_flow(ctx, output):
     #         print(f"\t{tree}")
     #         for leaf, samples in tree_data.items():
     #             print(f"\t\t{leaf}: {len(samples)}")
-    corr_per_leaf = compute_leaves_correlation(samples_per_leaves_dict)
+    
+    corr_per_leaf,corr_per_leaf_cleaned = compute_leaves_correlation(samples_per_leaves_dict)
+    scores = compute_leaves_score(corr_per_leaf)
+    # print("********* Value not cleaned")
+    # for k,v in scores.items():
+    #     if v > 0:
+    #         print(f'Leaf:({k[0]},{k[1]}) score: {v}')
+    print("********* VAlue cleaned")
+    scores2 = compute_leaves_score(corr_per_leaf_cleaned)
+    for k,v in scores2.items():
+        print(f'Leaf:({k[0]},{k[1]}) score: {v}')
     # for leaf,leaf_data in corr_per_leaf.items():
     #     print(f'**** Leaf {leaf} is correlated with : \n')
     #     for leaf_data,corr_value in leaf_data.items():
