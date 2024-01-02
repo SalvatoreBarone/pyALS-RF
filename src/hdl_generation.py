3f74658c128d63a1f3d02292d1feb6732fbfeb17
Salvatore Barone <salvatore.barone@unina.it>
2024-01-02 15:12:24 +0100

    (wip) elapsed time

--- src/hdl_generation.py
+++ src/hdl_generation.py
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License along with
 RMEncoder; if not, write to the Free Software Foundation, Inc., 51 Franklin
 Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """
-import json5, logging
+import json5, logging, time
 from distutils.dir_util import mkpath
 from pyamosa.Pareto import Pareto
 from .HDLGenerators.HDLGenerator import HDLGenerator
@@ -37,6 +37,11 @@ def hdl_generation(ctx, lut_tech, skip_exact : bool, output):
         mkpath(ctx.obj["configuration"].outdir)
     create_classifier(ctx)
     create_yshelper(ctx)
+    
+    elapsed = time.time_ns()
+    baseline_accuracy = ctx.obj["classifier"].evaluate_test_dataset()
+    elapsed = time.time_ns() - elapsed
+    logger.info(f"Baseline accuracy: {baseline_accuracy}%\nExact classifier took {elapsed} ns")
     
     if ctx.obj["flow"] is None:
         load_flow(ctx)
@@ -68,6 +73,12 @@ def hdl_generation(ctx, lut_tech, skip_exact : bool, output):
         logger.info(f"Expected LUT savings for BNs: {(1 - ax_luts_bns / exact_luts_bns) * 100}%"
                     f"\n\tExpected LUT savings for DBs: {(1 - ax_luts_dbs / exact_luts_dbs) * 100}%"
                     f"\n\tExpected FFs savings for DBs: {(1 - ax_ffs_dbs / exact_ffs_dbs) * 100}%")
+        elapsed = time.time_ns()
+        accuracy = ctx.obj["classifier"].evaluate_test_dataset()
+        elapsed = time.time_ns() - elapsed
+        logger.info(f"Accuracy: {accuracy}%\Ax. classifier took {elapsed} ns")
+    
+        
     elif ctx.obj["flow"] == "ps":
         if "pareto_front" not in ctx.obj:
             create_problem(ctx, mode = "full")
@@ -79,18 +90,18 @@ def hdl_generation(ctx, lut_tech, skip_exact : bool, output):
             ctx.obj["pareto_front"] = ctx.obj["optimizer"].archive
         hdl_generator = PsHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
         hdl_generator.generate_axhdl(pareto_set = ctx.obj['pareto_front'].get_set(), enable_espresso = ctx.obj['espresso'], lut_tech = lut_tech)
-    elif ctx.obj["flow"] == "als-onestep":
-        hdl_generator = SingleStepAlsHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
-    elif ctx.obj["flow"] == "als-twosteps":
-        hdl_generator = TwoStepsAlsHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
-    elif ctx.obj["flow"] == "wcals-onestep":
-        hdl_generator = SingleStepAlsWcHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
-    elif ctx.obj["flow"] == "wcals-twosteps":
-        hdl_generator = TwoStepsAlsWcHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
-    elif ctx.obj["flow"] == "full-onestep":
-        hdl_generator = SingleStepFullHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
-    elif ctx.obj["flow"] == "full-twosteps":
-        hdl_generator = TwoStepsFullHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
+    # elif ctx.obj["flow"] == "als-onestep":
+    #     hdl_generator = SingleStepAlsHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
+    # elif ctx.obj["flow"] == "als-twosteps":
+    #     hdl_generator = TwoStepsAlsHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
+    # elif ctx.obj["flow"] == "wcals-onestep":
+    #     hdl_generator = SingleStepAlsWcHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
+    # elif ctx.obj["flow"] == "wcals-twosteps":
+    #     hdl_generator = TwoStepsAlsWcHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
+    # elif ctx.obj["flow"] == "full-onestep":
+    #     hdl_generator = SingleStepFullHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
+    # elif ctx.obj["flow"] == "full-twosteps":
+    #     hdl_generator = TwoStepsFullHdlGenerator(ctx.obj["classifier"], ctx.obj["yshelper"], ctx.obj['configuration'].outdir)
     else:
         print(f"{ctx.obj['flow']}: unrecognized approximation flow. Bailing out.")
         exit()
